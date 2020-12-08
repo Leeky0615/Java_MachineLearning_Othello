9ae9d0ea6c129175b6756336199c3bb212fdccc1
Leeky0615 <rbdus7174@naver.com>
2020-12-09 06:17:45 +0900

    .

--- MachineLearning-Othello/src/othello/Main.java
+++ MachineLearning-Othello/src/othello/Main.java
@@ -1,6 +1,7 @@
 package othello;
 
 import java.awt.Point;
+import java.util.Vector;
 
 import othello.Constants.EState;
 import othello.Constants.EStone;
@@ -12,15 +13,88 @@ public class Main {
 		oMap.init();
 		Othello othello = new Othello(oMap);
 		othello.init();
-		othello.countStones();
-		System.out.println("=======================");
-		othello.putStone(new Point(3,2));
-		System.out.println("=======================");
-		othello.putStone(new Point(2,4));
-		System.out.println("=======================");
-		othello.putStone(new Point(3,5));
-		System.out.println("=======================");
-		othello.putStone(new Point(4,2));
+		while(true) {
+			othello.findPossible();
+			if(othello.getPossiblePoints().size()==0) {
+				othello.setCurrentS(othello.getCurrentS());
+				othello.findPossible();
+			}
+			othello.putStone(othello.getPossiblePoints().get(0));
+		}
+		
+//		int[][] asd = null;
+//		for (int j = 0; j < 9; j++) {
+//			System.out.println(othello.getCurrentS());
+//			othello.findPossible();
+//			othello.putStone(othello.getPossiblePoints().get(0));
+//			if(j==8) asd = othello.getMap();
+//		}
+//		System.out.println("=========");
+//		othello.setMap(asd);
+//		othello.drawMap();
+//		Point p = new Point(5,4);
+//		System.out.println(othello.isEmpty(p));;
+//		Vector<Point> psPs = othello.possibleSurroundingP(p);
+//		System.out.println(psPs);
+//		for(Point psP : psPs) {
+//			System.out.println(psP);
+//			Point ptoCheck = new Point((2*psP.x)-p.x, (2*psP.y)-p.y);
+//			System.out.println(ptoCheck);
+//			System.out.println(othello.isInMap(ptoCheck)&&!othello.isEmpty(ptoCheck));
+//			System.out.println(othello.getStoneColor(ptoCheck) == othello.getCurrentS());
+//			othello.addPoint(psP,ptoCheck);
+//			System.out.println(othello.getAvailablechangePs().size());
+//			System.out.println(othello.getPossiblePoints().size());
+//		}
+		
+		
+		
+//		System.out.println("Step0=========================");
+//		othello.drawMap();
+//		System.out.println("Step1=========================");
+//		othello.findPossible();
+//		othello.putStone(new Point(3,2));
+//		System.out.println("Step2=========================");
+//		othello.findPossible();
+//		othello.putStone(new Point(2,2));
+//		System.out.println("Step3=========================");
+//		othello.findPossible();
+//		othello.putStone(new Point(1,2));
+//		System.out.println("Step4=========================");
+//		othello.findPossible();
+//		othello.putStone(new Point(1,1));
+//		System.out.println("Step5=========================");
+//		othello.findPossible();
+//		Vector<Point> points = new Vector<Point>();
+//		for(EState eState: EState.values()) {
+//			Point newP = new Point(1 + eState.getX(), 0 + eState.getY());
+//			System.out.println(newP);
+//			// 끝 값들은 제외 && 현재 놓는 돌의 색과 다르면 벡터에 널기
+////			if(newP.x >= 0 && newP.y >= 0 ) {
+////				
+////			}
+//			if(othello.isInMap(newP) && !othello.isEmpty(newP) && othello.getMap()[newP.x][newP.y] ==1){
+//				points.add(newP);
+//			}
+//		}
+//		System.out.println(points);
+//		othello.possibleSurroundingP(new Point(1,0));
+//		System.out.println("Step7=========================");
+//		othello.findPossible();
+//		othello.putStone(new Point(1,2));
+//		othello.putStone(new Point(2,2));
+//		othello.findPossible();
+//		System.out.println("=======================");
+//		othello.putStone(new Point(2,4));
+//		System.out.println("=======================");
+//		othello.putStone(new Point(3,5));
+//		System.out.println("=======================");
+//		othello.putStone(new Point(4,2));
+//		System.out.println("=======================");
+//		othello.putStone(new Point(4,5));
+//		System.out.println("=======================");
+//		othello.putStone(new Point(5,4));
+//		System.out.println("=======================");
 		
 	}
 
--- MachineLearning-Othello/src/othello/Othello.java
+++ MachineLearning-Othello/src/othello/Othello.java
@@ -7,23 +7,45 @@ import othello.Constants.EState;
 import othello.Constants.EStone;
 
 public class Othello {
+	private int playtime;
 	private EStone currentS;
 	private EStone otherS;
 	private OMap oMap;
 	private int[][] map;
 	private Vector<Point> availablechangePs;
+	private Vector<Point> possiblePoints;
+	private Vector<int[][]> playBoards;
 	
-	public Othello(OMap oMap) {
-		this.oMap = oMap;
-	}
+	public Othello(OMap oMap) {this.oMap = oMap;}
+	public Vector<Point> getPossiblePoints() {return possiblePoints;}
+	public void setPossiblePoints(Vector<Point> possiblePoints) {this.possiblePoints = possiblePoints;}
+	public int[][] getMap() {return this.copyMap(map);}
+	public void setMap(int[][] map) {this.map = this.copyMap(map);}
+	public EStone getOtherS() {return otherS;}
+	public void setOtherS(EStone otherS) {this.otherS = otherS;}
+	public EStone getCurrentS() {return currentS;}
+	public Vector<Point> getAvailablechangePs() {return availablechangePs;}
+	public void setAvailablechangePs(Vector<Point> availablechangePs) {this.availablechangePs = availablechangePs;}
 
+	
 	public void init() {
 		this.currentS = EStone.Black;
 		this.otherS = EStone.White;
 		this.oMap.init();
 		this.map = oMap.getMap();
 		this.availablechangePs = new Vector<Point>();
+		this.possiblePoints = new Vector<Point>();
+		this.playBoards = new Vector<int[][]>();
+		this.playBoards.add(copyMap(this.map));
 	}
+	public int[][] copyMap(int[][] map){
+		int[][] copyMap = new int[map.length][map[0].length];
+		for (int i = 0; i < copyMap.length; i++) {
+			System.arraycopy(map[i], 0, copyMap[i], 0, map[0].length);
+		}
+		return copyMap;
+	}
+		
 	// 놓는 돌의 색 초기화
 	public void setCurrentS(EStone eStone) {
 		this.otherS = currentS;
@@ -40,8 +62,8 @@ public class Othello {
 	}
 	// 포인트가 Map을 벗어나는 위치인지 확인.
 	public Boolean isInMap(Point P) {
-		if(P.x > 0 && P.x < 7) {
-			if(P.y > 0 && P.y < 7) return true;
+		if(P.x >= 0 && P.x < 8) {
+			if(P.y >= 0 && P.y < 8) return true;
 			else return false;
 		}else return false;
 	}
@@ -50,35 +72,36 @@ public class Othello {
 		return EStone.values()[this.map[p.x][p.y]];
 	}
 	// 놓는 자리의 주변에 놓을 수 있는 포인트들 담기(주변에 다른색의 돌만 있으면 가능) -> 1차 확인
-	public Vector<Point> possibleSurroundingP(Point p) {
+	public Vector<Point> possibleSurroundingP(Point currentP) {
 		// 새로 놓는 자리에 8방향의 포인트 넣기
 		Vector<Point> points = new Vector<Point>();
 		for(EState eState: EState.values()) {
-			Point newP = new Point(p.x + eState.getX(), p.y + eState.getY());
+			Point newP = new Point(currentP.x + eState.getX(), currentP.y + eState.getY());
 			// 끝 값들은 제외 && 현재 놓는 돌의 색과 다르면 벡터에 널기
-			if(isEmpty(newP) && isInMap(newP) && map[newP.x][newP.y] != this.currentS.ordinal()){
+			if(isInMap(newP) && !isEmpty(newP) && map[newP.x][newP.y] == this.otherS.ordinal()){
 				points.add(newP);
 			}
 		}
 		return points;
 	}
 	
-	public void addPoint(Point currentP, Point possibleP) {
-		Point ptoCheck = new Point((2*currentP.x)-possibleP.x, (2*currentP.y)-possibleP.y);
-		if(!isEmpty(ptoCheck) && isInMap(ptoCheck)) {
-			// 대칭되는 돌이 놓으려고 하는 돌과 다르면 다시 대칭되는 돌 확인. 
+	public void addPoint(Point currentP, Point psP) {
+		Point ptoCheck = new Point((2*psP.x)-currentP.x, (2*psP.y)-currentP.y);
+		if(isInMap(ptoCheck)&& !isEmpty(ptoCheck)) {
+			// 놓는 돌이랑 같이 뒤집혀 질 수 있는 반대편의 돌들을 벡터에 담음. 
 			if(this.getStoneColor(ptoCheck) == this.currentS) this.availablechangePs.add(ptoCheck);
-			else addPoint(ptoCheck, currentP);
+			else addPoint(psP, ptoCheck);
 		}
 	}
 	
 	// 1차 확인된 돌을 중심으로 대칭되는 돌이 놓으려고하는 돌과 같은지 비교해 돌을 놓았을 때 뒤집힐 수 있는 대칭되는 돌을 담음.
-	public void possiblesPoints(Point p) {
-		Vector<Point> possiblePs = possibleSurroundingP(p);
-		for(Point possibleP : possiblePs) {
-			addPoint(p,possibleP);
-		}
+	public void possiblesPoints(Point currentP) {
+		try {
+			Vector<Point> psPs = possibleSurroundingP(currentP);
+			for(Point psP : psPs) {addPoint(currentP,psP);}
+		}catch (ArrayIndexOutOfBoundsException e) {}
 	}
+	
 	// 돌 뒤집기.
 	private void changeStones(Point p) {
 		for(Point oppsiteP : this.availablechangePs) {
@@ -103,7 +126,6 @@ public class Othello {
 					if(y<oppsiteP.y) state = EState.ES;
 					else state = EState.NW;
 				}
-				
 				for (int i = x; i <= Math.max(oppsiteP.x, p.x); i++) {
 					if(state == EState.ES) {
 						this.map[i][y++] = this.currentS.ordinal();
@@ -114,28 +136,74 @@ public class Othello {
 			}
 		}
 	}
+	public Boolean checkStone(Point p) {
+		// 놓는 곳이 빈곳이면
+		if (isEmpty(p)) {
+			// 
+			this.possiblesPoints(p);
+			if(this.availablechangePs.size() != 0) {
+				this.possiblePoints.add(p);
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return false;
+		}
+	}
+
 	public void putStone(Point p) {
-		this.clearVector();
-		if (isEmpty(p)) this.possiblesPoints(p);
+		this.possiblesPoints(p);
 		this.changeStones(p);
-		this.countStones();
+		this.drawMap();
+		this.clearVector();
+		this.possiblePoints = new Vector<Point>();
 		this.setCurrentS(this.otherS);
 	}
 	
-	public void countStones() {
-		int black = 0;
-		int white = 0;
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map.length; j++) {
-				System.out.print(this.map[j][i]);
-				if(this.map[i][j]==EStone.Black.ordinal()) black++;
-				else if(this.map[i][j]==EStone.White.ordinal()) white++;
+	public void countPoints() {
+		int point = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if(this.currentS == EStone.Black) {
+					if(this.map[j][i]==EStone.Black.ordinal()) point++;
+					else if(this.map[j][i]==EStone.White.ordinal()) point--;
+				}else {
+					if(this.map[j][i]==EStone.Black.ordinal()) point--;
+					else if(this.map[j][i]==EStone.White.ordinal()) point++;
+				}
 			}
-			System.out.println();
 		}
-		System.out.println("흑돌 : "+black+"개 / 흰돌 : "+white+"개");
+		System.out.println("점수: "+point);
 	}
 	
-
-	
+	public void findPossible() {
+		int[][] originMap = copyMap(this.map);
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				Point p = new Point(j, i);
+				if(checkStone(p)) {
+					System.out.print("좌표: (" + j + ", " + i + ") / ");
+					this.changeStones(p);
+					this.countPoints();
+					System.out.println("- - - - - - - - - - - - -");
+					this.map = copyMap(originMap);
+					this.clearVector();
+				}
+			}
+		}
+		System.out.println("놓을 수 있는 포인트 개수  : " + this.possiblePoints.size());
+		if (this.possiblePoints.size() == 0) {
+			this.setCurrentS(this.otherS);
+		}
+	}
+	public void drawMap() {
+		int[][] originMap = this.map;
+		for (int i = 0; i < originMap.length; i++) {
+			for (int j = 0; j < originMap.length; j++) {
+				System.out.print(originMap[j][i]);
+				}
+			System.out.println();
+		}
+	}
 }
